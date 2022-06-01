71bde70b4f90db89b8f028fcd7486d0a15998ae5
PhucTai2007 <Phuctaitai2007@gmail.com>
2022-06-01 16:56:35 +0700

    first commit

--- login.php
+++ login.php
@@ -1,4 +1,25 @@
 <?php
-    
+	$value=$_POST["username"];
+	setcookie("username", "$value");
+	session_start();
+	//Connect
+	$conn = mysqli_connect("localhost","root","","quanlyhethong");
+	if (isset($_POST['username'])) {
+		$username=$_POST['username'];
+		$password=($_POST['password']);
+		$sql= "SELECT username,password FROM user WHERE username='".$username."' AND password='".$password."'limit 1";
+		
+        $result = $conn->query($sql);
 
+		if ($result->num_rows == 1) {
+			  header("location:index.html");
+		}else{
+			echo "Bạn chưa đăng nhập!";
+		}
+	}
+    // $conn = mysqli_connect("localhost", "root", "","quanlyhethong");
+    // if(!$conn){
+    //     die("khong thanh cong". mysqli_connect_error());
+    // }
+    // echo"thang cong";
 ?>
