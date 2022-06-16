1179836d629f709c536e70fe0d4eea020415c6d0
ngoc130198 <106042384+ngoc130198@users.noreply.github.com>
2022-06-16 08:41:51 +0700

    Delete connectQLUSER.php

--- connectQLUSER.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","quanlyhethong");
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-?>
